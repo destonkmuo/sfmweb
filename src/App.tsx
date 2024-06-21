bb1b592b26f0bf7a10ecf56170345f0026f037f5
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-06-21 17:30:19 -0500

    Added Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,250 @@
 import React from 'react';
-import './App.css';
+import './home/styles/Home.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <html data-wf-domain="escape-free-template.webflow.io" data-wf-page="5e4b1929fccc7f5af9de826b" data-wf-site="5e4b1929fccc7f2575de825e" data-wf-status="1">
+       <head>
+          <title>Sure Foundation Ministries</title>
+          <meta content="width=device-width, initial-scale=1" name="viewport"/>
+          <meta content="Webflow" name="generator"/>
+          <link href="https://fonts.googleapis.com" rel="preconnect"/>
+          <link href="https://fonts.gstatic.com" rel="preconnect" cross-origin="anonymous"/>
+          <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+          <link href="https://assets.website-files.com/img/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+          <link href="https://assets.website-files.com/img/webclip.png" rel="apple-touch-icon"/>
+       </head>
+       <body>
+          <div>
+             <div data-collapse="tiny" data-animation="over-right" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" className="navbar w-nav">
+                <div className="w-container">
+                   <a href="/" aria-current="page" className="logo w-nav-brand w--current">
+                      <div className="long-text">Sure Foundation Ministries</div>
+                   </a>
+                   <nav role="navigation" className="nav-menu w-nav-menu"><a href="/" aria-current="page" className="nav-link w-nav-link w--current">Home</a><a href="/categories" className="nav-link w-nav-link">Categories</a><a href="/about" className="nav-link w-nav-link">About</a><a href="/contact" className="nav-link w-nav-link">Contact</a><a href="#" className="close-x w-hidden-main w-hidden-medium w-hidden-small">×</a></nav>
+                   <div className="menu-button w-hidden-main w-hidden-medium w-hidden-small w-clearfix w-nav-button">
+                      <div className="menu-icon w-icon-nav-menu"></div>
+                      <div className="menu-link">MENU</div>
+                   </div>
+                </div>
+             </div>
+             <div className="hero-section">
+                <div className="hero-content w-container">
+                   <div className="hero-heading">Let&#x27;s do it together.</div>
+                   <div className="hero-subheading">We travel the world in search of stories. Come along for the ride.</div>
+                   <a href="#Latest-post" className="button w-button">View Latest Posts</a>
+                </div>
+             </div>
+             <div className="category-section">
+                <div className="w-container">
+                   <div className="w-dyn-list">
+                      <div role="list" className="w-clearfix w-dyn-items w-row">
+                         <div role="listitem" className="category-wrapper w-dyn-item w-col w-col-2"><a href="/categories/nature" className="category-link">Nature</a></div>
+                         <div role="listitem" className="category-wrapper w-dyn-item w-col w-col-2"><a href="/categories/photography" className="category-link">Photography</a></div>
+                         <div role="listitem" className="category-wrapper w-dyn-item w-col w-col-2"><a href="/categories/relaxation" className="category-link">Relaxation</a></div>
+                         <div role="listitem" className="category-wrapper w-dyn-item w-col w-col-2"><a href="/categories/vacation" className="category-link">Vacation</a></div>
+                         <div role="listitem" className="category-wrapper w-dyn-item w-col w-col-2"><a href="/categories/travel" className="category-link">Travel</a></div>
+                         <div role="listitem" className="category-wrapper w-dyn-item w-col w-col-2"><a href="/categories/adventure" className="category-link">Adventure</a></div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+             <div id="posts" className="main-section bottom-padding">
+                <div id="Latest-post" className="w-container">
+                   <div className="section-heading">
+                      <h2 className="section-title">Featured Posts</h2>
+                      <div className="med-divider"></div>
+                   </div>
+                   <div className="w-dyn-list">
+                      <div role="list" className="w-clearfix w-dyn-items w-row">
+                         <div role="listitem" className="featuredthumbnail w-dyn-item w-col w-col-6">
+                            <a href="/blog/this-is-my-new-post" data-w-id="3c1b0055-dd47-cc77-5812-26c30ac7c948" className="featured-wrapper w-inline-block">
+                               <div>
+                                  <div className="category-tag-2">Photography</div>
+                                  <div className="featured-image"></div>
+                               </div>
+                               <div className="featured-text">
+                                  <div className="featured-title">The Road Ahead</div>
+                                  <div className="featured-description">The road ahead might be paved - it might not be.</div>
+                                  <div className="featured-details">
+                                     <div className="w-clearfix">
+                                        <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7ff9f8de8335_128-14.jpg" className="author-img"/>
+                                        <div className="author-title lite">Mat Vogels</div>
+                                        <div className="thumbnail-date lite">September 25, 2015</div>
+                                     </div>
+                                  </div>
+                               </div>
+                            </a>
+                         </div>
+                         <div role="listitem" className="featuredthumbnail w-dyn-item w-col w-col-6">
+                            <a href="/blog/from-top-down" data-w-id="3c1b0055-dd47-cc77-5812-26c30ac7c948" className="featured-wrapper w-inline-block">
+                               <div>
+                                  <div className="category-tag-2">Adventure</div>
+                                  <div className="featured-image"></div>
+                               </div>
+                               <div className="featured-text">
+                                  <div className="featured-title">From Top Down</div>
+                                  <div className="featured-description">Once a year, go someplace you’ve never been before.</div>
+                                  <div className="featured-details">
+                                     <div className="w-clearfix">
+                                        <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7f7389de832c_128-34.jpg" className="author-img"/>
+                                        <div className="author-title lite">William Wong</div>
+                                        <div className="thumbnail-date lite">September 25, 2015</div>
+                                     </div>
+                                  </div>
+                               </div>
+                            </a>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div className="w-container">
+                   <div className="section-heading">
+                      <h2 className="section-title">Most Recent</h2>
+                      <div className="med-divider"></div>
+                   </div>
+                   <div className="w-dyn-list">
+                      <div role="list" className="w-clearfix w-dyn-items w-row">
+                         <div role="listitem" className="blog-thumbnail w-dyn-item w-col w-col-4">
+                            <a href="/blog/still-standing-tall" data-w-id="46150442-4efa-9d36-3a4c-20d527e7a6bc" className="thumbnail-wrapper w-inline-block">
+                               <div className="image-wrapper">
+                                  <div className="thumbnail-image"></div>
+                                  <div className="category-tag">Nature</div>
+                               </div>
+                               <div className="thumbnail-text">
+                                  <div className="blog-title">Still Standing Tall</div>
+                                  <div className="preview-text">Life begins at the end of your comfort zone.</div>
+                               </div>
+                               <div className="thumb-details w-clearfix">
+                                  <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7f7389de832c_128-34.jpg" className="author-img"/>
+                                  <div className="author-title">William Wong</div>
+                                  <div className="thumbnail-date">9/25/2015</div>
+                               </div>
+                            </a>
+                         </div>
+                         <div role="listitem" className="blog-thumbnail w-dyn-item w-col w-col-4">
+                            <a href="/blog/sunny-side-up" data-w-id="46150442-4efa-9d36-3a4c-20d527e7a6bc" className="thumbnail-wrapper w-inline-block">
+                               <div className="image-wrapper">
+                                  <div className="thumbnail-image"></div>
+                                  <div className="category-tag">Photography</div>
+                               </div>
+                               <div className="thumbnail-text">
+                                  <div className="blog-title">Sunny Side Up</div>
+                                  <div className="preview-text">No place is ever as bad as they tell you it’s going to be.</div>
+                               </div>
+                               <div className="thumb-details w-clearfix">
+                                  <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7ff9f8de8335_128-14.jpg" className="author-img"/>
+                                  <div className="author-title">Mat Vogels</div>
+                                  <div className="thumbnail-date">9/25/2015</div>
+                               </div>
+                            </a>
+                         </div>
+                         <div role="listitem" className="blog-thumbnail w-dyn-item w-col w-col-4">
+                            <a href="/blog/water-falls" data-w-id="46150442-4efa-9d36-3a4c-20d527e7a6bc" className="thumbnail-wrapper w-inline-block">
+                               <div className="image-wrapper">
+                                  <div className="thumbnail-image"></div>
+                                  <div className="category-tag">Relaxation</div>
+                               </div>
+                               <div className="thumbnail-text">
+                                  <div className="blog-title">Water Falls</div>
+                                  <div className="preview-text">We travel not to escape life, but for life not to escape us.</div>
+                               </div>
+                               <div className="thumb-details w-clearfix">
+                                  <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7ff9f8de8335_128-14.jpg" className="author-img"/>
+                                  <div className="author-title">Mat Vogels</div>
+                                  <div className="thumbnail-date">9/25/2015</div>
+                               </div>
+                            </a>
+                         </div>
+                         <div role="listitem" className="blog-thumbnail w-dyn-item w-col w-col-4">
+                            <a href="/blog/through-the-mist" data-w-id="46150442-4efa-9d36-3a4c-20d527e7a6bc" className="thumbnail-wrapper w-inline-block">
+                               <div className="image-wrapper">
+                                  <div className="thumbnail-image"></div>
+                                  <div className="category-tag">Vacation</div>
+                               </div>
+                               <div className="thumbnail-text">
+                                  <div className="blog-title">Through the Mist</div>
+                                  <div className="preview-text">Travel makes you see what a tiny place you occupy in the world.</div>
+                               </div>
+                               <div className="thumb-details w-clearfix">
+                                  <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7f7389de832c_128-34.jpg" className="author-img"/>
+                                  <div className="author-title">William Wong</div>
+                                  <div className="thumbnail-date">9/25/2015</div>
+                               </div>
+                            </a>
+                         </div>
+                         <div role="listitem" className="blog-thumbnail w-dyn-item w-col w-col-4">
+                            <a href="/blog/awaken-early" data-w-id="46150442-4efa-9d36-3a4c-20d527e7a6bc" className="thumbnail-wrapper w-inline-block">
+                               <div className="image-wrapper">
+                                  <div className="thumbnail-image"></div>
+                                  <div className="category-tag">Vacation</div>
+                               </div>
+                               <div className="thumbnail-text">
+                                  <div className="blog-title">Awaken Early</div>
+                                  <div className="preview-text">Not all those who wander are lost.</div>
+                               </div>
+                               <div className="thumb-details w-clearfix">
+                                  <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7ff9f8de8335_128-14.jpg" className="author-img"/>
+                                  <div className="author-title">Mat Vogels</div>
+                                  <div className="thumbnail-date">9/25/2015</div>
+                               </div>
+                            </a>
+                         </div>
+                         <div role="listitem" className="blog-thumbnail w-dyn-item w-col w-col-4">
+                            <a href="/blog/try-it-always" data-w-id="46150442-4efa-9d36-3a4c-20d527e7a6bc" className="thumbnail-wrapper w-inline-block">
+                               <div className="image-wrapper">
+                                  <div className="thumbnail-image"></div>
+                                  <div className="category-tag">Travel</div>
+                               </div>
+                               <div className="thumbnail-text">
+                                  <div className="blog-title">Try it Always</div>
+                                  <div className="preview-text">The world is a book, and those who do not travel read only one page.</div>
+                               </div>
+                               <div className="thumb-details w-clearfix">
+                                  <img alt="" src="https://assets.website-files.com/5e4b1929fccc7f96f6de825d/5e4b1929fccc7ff9f8de8335_128-14.jpg" className="author-img"/>
+                                  <div className="author-title">Mat Vogels</div>
+                                  <div className="thumbnail-date">9/25/2015</div>
+                               </div>
+                            </a>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+             <div className="main-section dark">
+                <div className="w-container">
+                   <div className="section-heading">
+                      <h2 className="white">Stay in Touch</h2>
+                      <div className="med-divider"></div>
+                   </div>
+                   <div className="form-wrapper w-form">
+                      <form id="email-form" name="email-form" data-name="Email Form" method="get" className="w-clearfix" data-wf-page-id="5e4b1929fccc7f5af9de826b" data-wf-element-id="3bd33d45-8464-92a7-9cf0-d7b045aa296d"><input className="field w-input" max-length="256" name="email" data-name="Email" placeholder="Enter your email address" type="email" id="email" required/><input type="submit" data-wait="Please wait..." className="submit-button w-button" value="Submit"/></form>
+                      <div className="success-message w-form-done">
+                         <p>Thank you! Your submission has been received!</p>
+                      </div>
+                      <div className="w-form-fail">
+                         <p>Oops! Something went wrong while submitting the form</p>
+                      </div>
+                   </div>
+                </div>
+                <div className="footer-section">
+                   <div className="w-container">
+                      <div className="w-row">
+                         <div className="w-col w-col-4 w-col-small-4">
+                            <a href="/" aria-current="page" className="footer-logo w-inline-block w--current">
+                               <div className="long-text">Escape.</div>
+                            </a>
+                         </div>
+                         <div className="footer-links w-col w-col-8 w-col-small-8"><a href="/" aria-current="page" className="footer-link w--current">Home</a><a href="/categories" className="footer-link">Categories</a><a href="/about" className="footer-link">About</a><a href="/contact" className="footer-link">Contact</a></div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+          <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5e4b1929fccc7f2575de825e" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" cross-origin="anonymous"></script><script src="https://assets.website-files.com/5e4b1929fccc7f2575de825e/js/webflow.073a8a568.js" type="text/javascript"></script>
+       </body>
+    </html>
+  )
 }
 
 export default App;
